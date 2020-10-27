fe05d3b9005714ea8849ef11e2e9ae64c3fbf986
jomariepolanco <jomariepolanco@gmail.com>
2020-10-27 12:44:23 -0400

    review

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
-  
-  def new
-  end
+    def index
+        @posts = Post.all 
 
-  def create
-    Post.create(title: params[:post][:title], description: params[:post][:description])
-    redirect_to posts_path
-  end
+    end
+
+    def new
+    end
+
+    def create
+        Post.create(title: params[:post][:title], description: params[:post][:description])
+        redirect_to posts_path
+    end
 end
